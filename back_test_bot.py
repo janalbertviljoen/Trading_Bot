7dc58d370d1e9a25376de76811ed0bca628c3459
Jan-Albert <javiljoen74@gmail.com>
2026-01-31 13:56:09 +0200

    added trading functions

--- back_test_bot.py
+++ back_test_bot.py
@@ -3,12 +3,10 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
-# ----------------------------
-# 1. Fetch historical BTC data from Binance
-# ----------------------------
 BINANCE_URL = "https://api.binance.com/api/v3/klines"
 
 
+# TODO: Split to seperate file: "read_data.py"
 def fetch_btc_history(interval="1h", start_date="2024-01-01", symbol="BTCUSDT"):
     """
     Fetch historical BTC price data from Binance.
@@ -39,7 +37,7 @@ def fetch_btc_history(interval="1h", start_date="2024-01-01", symbol="BTCUSDT"):
             break
 
         all_data.extend(data)
-        start_ts = data[-1][0] + 1  # move forward safely
+        start_ts = data[-1][0] + 1
 
     df = pd.DataFrame(
         all_data,
@@ -60,6 +58,7 @@ def fetch_btc_history(interval="1h", start_date="2024-01-01", symbol="BTCUSDT"):
     )
 
     df["timestamp"] = pd.to_datetime(df["close_time"], unit="ms", utc=True)
+    df["timestamp"] = df["timestamp"].dt.floor("s").dt.tz_localize(None)
     df["price"] = df["close"].astype(float)
     df["volume"] = df["volume"].astype(float)
     df["high"] = df["high"].astype(float)
@@ -68,6 +67,7 @@ def fetch_btc_history(interval="1h", start_date="2024-01-01", symbol="BTCUSDT"):
     return df[["timestamp", "high", "low", "price", "volume"]].reset_index(drop=True)
 
 
+# TODO: Split to seperate file: "indicators.py"
 def rolling_vwap(df, price_col="price", volume_col="volume", window=50):
     """
     Computes the volume-weighted average price (VWAP) over a rolling window.
@@ -87,7 +87,7 @@ def rolling_vwap(df, price_col="price", volume_col="volume", window=50):
     return vwap
 
 
-def detect_large_moves(df, unit_price="price", metric_name="vwap", threshold=0.03):
+def detect_deviation(df, unit_price="price", metric_name="vwap", threshold=0.03):
     """
     Detects significant price deviations from metric, uses volume-weighted average price as default.
 
@@ -104,6 +104,55 @@ def detect_large_moves(df, unit_price="price", metric_name="vwap", threshold=0.0
     return df
 
 
+# TODO: Split to seperate file: "trading_logic.py"
+def update_wallet(df, wallet_value: float, wallet_fraction=0.01):
+    """
+    Initialise wallet with USD amount. Update wallet across candels based on trade signals.
+
+    :param df (pandas.DataFrame): DataFrame containing 'price' and 'direction' columns
+    :param wallet_value (float): Initial wallet value in base currency
+    :param wallet_fraction (float): Fraction of wallet value to use for each trade (default is 0.01)
+    """
+    df.loc[0, "wallet_volume"] = price_to_unit_conversion(
+        wallet_value, df.loc[0]["price"]
+    )
+    for i in range(1, len(df)):
+        df.loc[i, "trade_volume"] = trade_volume(
+            df.loc[i]["direction"], df.loc[i - 1]["wallet_volume"]
+        )
+        df.loc[i, "wallet_volume"] = (
+            df.loc[i - 1, "wallet_volume"] + df.loc[i, "trade_volume"]
+        )
+    return df
+
+
+def price_to_unit_conversion(trade_value: float, unit_price: float):
+    """
+    Converts transaction values to units based on specified unit price.
+
+    :param trade_value (float): The value of the trade in base currency
+    :param unit_price (float): The price per unit in base currency
+
+    :return (float): The number of units for the given trade value and unit price
+    """
+    units = trade_value / unit_price
+    return units
+
+
+def trade_volume(direction: int, wallet_volume: float, wallet_fraction: float):
+    """
+    Assigns trade volume based on trade signal and value of wallet.
+
+    :param direction (int): The direction of the trade: 1 for Buy, -1 for Sell, 0 for No Trade
+    :param wallet_volume (float): Total volume of the trading wallet
+    :param wallet_fraction (float): Fraction of total wallet value to use for each trade (default is 0.01)
+
+    :return (float): The volume of the trade in units
+    """
+    trade_volume = direction * wallet_fraction * wallet_volume
+    return trade_volume
+
+
 def trade_direction(df):
     """
     Determines if a trade should be a 'Buy' or 'Sell' based on deviation sign and expected direction of market price.
@@ -112,27 +161,45 @@ def trade_direction(df):
 
     :return (pandas.DataFrame): DataFrame with added 'trade' column indicating direction of trade: 1 for Buy, -1 for Sell, 0 for No Trade
     """
-    df["trade"] = 0
-    df.loc[(df["signal"]) & (df["deviation"] > 0), "trade"] = -1
-    df.loc[(df["signal"]) & (df["deviation"] < 0), "trade"] = 1
+    df["direction"] = 0
+    df.loc[(df["signal"]) & (df["deviation"] > 0), "direction"] = -1
+    df.loc[(df["signal"]) & (df["deviation"] < 0), "direction"] = 1
     return df
 
 
-def trade_volume(df, unit_price="price", wallet_fraction=0.01):
+def trade_value(
+    df,
+    unit_price: str = "price",
+    trade_volume: str = "trade_volume",
+    column_out="trade_value",
+):
     """
-    Assigns trade volume based on trade signal and value of wallet.
+    Calculates the trade value based on trade volume and unit price for all rows in the DataFrame.
 
-    :param df (pandas.DataFrame): DataFrame containing 'trade' column
-    :param unit_price (str): Name of the price column to use for calculating trade volume (default is "price")
-    :param wallet_fraction (float): Fraction of total wallet value to use for each trade (default is 0.01)
+    :param df (pandas.DataFrame): DataFrame containing 'trade_volume' and 'price' columns
+    :param unit_price (str): Name of the price column to use for calculating trade value (default is "price")
+    :param trade_volume_col (str): Name of the trade volume column (default is "trade_volume")
 
-    :return (pandas.DataFrame): DataFrame with added 'trade_volume' column
+    :return (float): The value of the trade in base currency for the given number of units and unit price
     """
-    df["trade_volume"] = wallet_fraction * df["signal"] * df["total_wallet_value"]
-    df["trade_volume_price"] = df[unit_price] * df["trade_volume"]
+    df[column_out] = unit_to_price_conversion(df[trade_volume], df[unit_price])
     return df
 
 
+def unit_to_price_conversion(trade_units: float, unit_price: float):
+    """
+    Converts transaction units to value based on specified unit price.
+
+    :param trade_units (float): The number of units being traded
+    :param unit_price (float): The price per unit in base currency
+
+    :return (float): The value of the trade in base currency for the given number of units and unit price
+    """
+    value = trade_units * unit_price
+    return value
+
+
+# TODO: Split to seperate file: "performance.py"
 def best_bot(df):
     """Copmutes the theoretical maximum profit possible given high and low prices in time interval selected.
 
