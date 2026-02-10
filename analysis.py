0d59f4988efe00d258cb28e357657a8b90dcb75b
Jan-Albert <javiljoen74@gmail.com>
2026-02-10 18:33:19 +0200

    Added analysis class

--- analysis.py
+++ analysis.py
@@ -12,6 +12,8 @@ class PromisedLandExplorer:
         self.params_trade = list(trader.trade_config_range.keys())
 
     def pre_process(self, metric="mean_normalised_wallet"):
+
+        ## Strategic Points
         df_sp = self.df_sp.sort_values(metric, ascending=False).copy()
 
         df_stat_d = self.calc_stat(df_sp, self.params_detect, metric)
@@ -26,9 +28,26 @@ class PromisedLandExplorer:
             df_sp["detect_weighted"] ** 2 + df_sp["trade_weighted"] ** 2
         )
 
+        ## Time series
+        df_ts = self.df_ts
+        highest_comb = self.df_sp.head(5)["coordinate"].reset_index(drop=True)
+        col_name = self.extract_ts_cols(self.df_ts, highest_comb)
+        df_ts_top = df_ts[["timestamp"] + col_name]
+        normalised_columns = list(
+            df_ts_top.columns[df_ts_top.columns.str.contains("normalised_wallet")]
+        )
+        df_ts_top = df_ts_top[["timestamp"] + normalised_columns]
+        df_ts_norm = df_ts[normalised_columns].div(df_ts["normalised_price"], axis=0)
+        df_ts_norm = df_ts_norm.merge(
+            df_ts[["timestamp"]], left_index=True, right_index=True
+        )
+
+        ## Store data tables
         self.df_sp = df_sp
         self.df_stat_d = df_stat_d
         self.df_stat_t = df_stat_t
+        self.df_ts_norm = df_ts_norm
+        self.highest_coor = highest_comb
 
     def calc_stat(self, df, params, metric):
         df_grp = (
